a60449c5eac47ed5282afbf601c2a528770ef8c0
Muramatsu-max36 <21cm0136@jec.ac.jp>
2022-09-16 15:12:22 +0900

    Initial Commit

---
+++ CombineProblem36/CombineProblem36App.swift
@@ -0,0 +1,17 @@
+//
+//  CombineProblem36App.swift
+//  CombineProblem36
+//
+//  Created by cmStudent on 2022/09/16.
+//
+
+import SwiftUI
+
+@main
+struct CombineProblem36App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CombineProblem36/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CombineProblem36
+//
+//  Created by cmStudent on 2022/09/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
